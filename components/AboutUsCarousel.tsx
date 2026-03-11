c47b1eaae574f06d66fd25cb16fd6f3538d1a780
barackj370-pixel <barackj370@gmail.com>
2026-03-11 14:37:22 +0300

    Update AboutUsCarousel.tsx

--- components/AboutUsCarousel.tsx
+++ components/AboutUsCarousel.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { ABOUT_US_DATA } from '../constants';
-import { Page } from '../types';
+import { Page } from '../sharedTypes';
 import { fetchPages } from '../services/supabaseService';
 
 const AboutUsCarousel: React.FC = () => {
